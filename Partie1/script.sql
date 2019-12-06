449332354a66e6c08d5a67f9c261027641760555
melchqt <57181754+melchqt@users.noreply.github.com>
2019-12-06 14:09:24 +0100

    exo

---
+++ Partie1/script.sql
@@ -0,0 +1,8 @@
+-- Partie 1
+-- Exercice 1 (créer une base de données languages sur phpMyAdmin)
+CREATE DATABASE `languages`;
+-- Creation de la DB si elle n'existe pas (bonne pratique!)
+CREATE DATABASE IF NOT EXISTS `languages`;
+-- exercice 2
+--Créer une base de données webDevelopment avec l’encodage UTF-8
+Selectionner UTF8 general_ci dans interclassement
